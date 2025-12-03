8bf5a2f15265b548164cf00c946a46a289556ddf
Miroslav Hýbler <miroslav@peko-studio.cz>
2025-12-03 10:44:34 +0100

    ⚙️ minSdk increased to 23, Dependencies versions bump

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
 
     defaultConfig {
         applicationId = "com.jet.tts.example"
-        minSdk = 21
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0.0-RC1"
