ac6028645b9d860b204d1ea695a7bd02dfccafa9
Miroslav Hýbler <miroslav@peko-studio.cz>
2026-02-03 19:34:27 +0100

    Dependencies versions bump, gradle 9.0.0

--- example-app/build.gradle.kts
+++ example-app/build.gradle.kts
@@ -32,9 +32,6 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
-    kotlinOptions {
-        jvmTarget = "11"
-    }
     kotlin {
         jvmToolchain(jdkVersion = 11)
     }
